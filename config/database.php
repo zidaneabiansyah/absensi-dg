e8e13106ca37364859a108801dd1b692c203d514
KalvinKL <kelvinlubis20@gmail.com>
2026-04-06 15:24:05 +0700

    feat: initialize database configuration file with default connection settings

--- config/database.php
+++ config/database.php
@@ -96,6 +96,7 @@ return [
             'prefix_indexes' => true,
             'search_path' => 'public',
             'sslmode' => env('DB_SSLMODE', 'prefer'),
+            'prepared' => env('DB_PREPARED_STATEMENTS', false),
         ],
 
         'sqlsrv' => [
